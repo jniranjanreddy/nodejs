b39511912b2d3febb9f9aa24f11fecbf8aff3658
niru <you@example.com>
2022-01-17 09:45:28 -0500

    lk

--- wiki-api/app.js
+++ wiki-api/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -23,10 +22,39 @@ const Article = mongoose.model("Article", articleSchema);
 
 app.get("/articles", function(req, res){
   Article.find(function(err, foundArticles){
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
     //console.log(foundArticles);
-    res.send(foundArticles)
+    //res.send(foundArticles)
   });
 });
+app.post("/articles", function(req, res){
+  console.log(req.body.title);
+  console.log(req.body.content);
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+
+  });
+newArticle.save();
+});
+
+
+app.delete("/articles", function(req, res){
+  Article.deleteMany(function(err){
+    if (!err){
+      res.send("Successfully deleted all the articles in wikiDB.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
   
