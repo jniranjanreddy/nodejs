871066d5bd8ef74a4d8f825e587c81dcebd31fe2
niru <you@example.com>
2022-01-18 07:50:43 -0500

    kj

--- wiki-api/app.js
+++ wiki-api/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -11,7 +12,9 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://192.168.9.100:27017/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true
+});
 
 const articleSchema = {
   title: String,
@@ -20,34 +23,103 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res){
-  Article.find(function(err, foundArticles){
-    if (!err) {
-      res.send(foundArticles);
+/////////////////////////All Articles///////////////////////////////////
+
+app.route("/articles")
+
+
+.get(function(req, res){
+  Article.find(function(err, articles){
+    if (articles) {
+      const jsonArticles = JSON.stringify(articles);
+      res.send(jsonArticles);
+    } else {
+      res.send("No articles currently in wikiDB.");
+    }
+  });
+})
+
+.post(function(req, res){
+  const newArticle = Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err){
+    if (!err){
+      res.send("Successfully added a new article.");
     } else {
       res.send(err);
     }
-    //console.log(foundArticles);
-    //res.send(foundArticles)
   });
-});
-app.post("/articles", function(req, res){
-  console.log(req.body.title);
-  console.log(req.body.content);
+})
 
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content
+.delete(function(req, res){
 
-  });
-newArticle.save();
-});
-
-
-app.delete("/articles", function(req, res){
   Article.deleteMany(function(err){
     if (!err){
       res.send("Successfully deleted all the articles in wikiDB.");
+    } else {
+      res.send(err);
+    }
+  });
+
+});
+
+
+/////////////////////////Individual Articles///////////////////////////////////
+
+app.route("/articles/:articleTitle")
+
+.get(function(req, res){
+  const articleTitle = req.params.articleTitle;
+  Article.findOne({title: articleTitle}, function(err, article){
+    if (article){
+      const jsonArticle = JSON.stringify(article);
+      res.send(jsonArticle);
+    } else {
+      res.send("No article with that title found.");
+    }
+  });
+})
+
+.patch(function(req, res){
+  const articleTitle = req.params.articleTitle;
+  Article.update(
+    {title: articleTitle},
+    {content: req.body.newContent},
+    function(err){
+      if (!err){
+        res.send("Successfully updated selected article.");
+      } else {
+        res.send(err);
+      }
+    });
+})
+
+.put(function(req, res){
+
+  const articleTitle = req.params.articleTitle;
+
+  Article.update(
+    {title: articleTitle},
+    {content: req.body.newContent},
+    {overwrite: true},
+    function(err){
+      if (!err){
+        res.send("Successfully updated the content of the selected article.");
+      } else {
+        res.send(err);
+      }
+    });
+})
+
+
+.delete(function(req, res){
+  const articleTitle = req.params.articleTitle;
+  LostPet.findOneAndDelete({title: articleTitle}, function(err){
+    if (!err){
+      res.send("Successfully deleted selected article.");
     } else {
       res.send(err);
     }
@@ -57,5 +129,4 @@ app.delete("/articles", function(req, res){
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
-  
 });
