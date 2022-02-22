8b9f757e95e5d03312aa89de45290024985b36c7
niru <you@example.com>
2022-02-21 21:01:42 -0500

    jk

---
+++ data-types/simple.js
@@ -0,0 +1,13 @@
+let age = 10; // Int datatype
+let firstName = "Niranjan" // String datatype
+let lastName = "Reddy"
+let fullAge = true; //boolean
+firstName = "Niranjan"
+// console.log(typeof fullAge) // in python we use print (type(fullAge))
+// console.log(typeof(firstName)) // a = "Rama" ; print(type(a))
+x = 10 + 5;
+x++;
+x--;
+let niru = 30;
+let sara = 25
+console.log( niru > sara) // python3 -c "niru = 30; sara = 25 ; print(niru > sara)"
--- varables.js
+++ varables.js
@@ -1,3 +1,7 @@
-a = "Hello"
-//console.log(a)
-print(a)
+string = "Hello"
+myInt = 10
+let firstName = "Niranjan"
+let lastName = "Jannapureddy"
+let middleName = "Reddy"
+console.log(firstName,middleName,lastName)
+
