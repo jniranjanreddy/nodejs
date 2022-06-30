5a00faacc32084ccc675356f53f1f5e22c1949fd
SG-Niranjan-Jannapureddy <you@example.com>
2022-06-30 11:19:40 -0400

    oq

---
+++ node-express-course/app.js
@@ -0,0 +1,10 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+// const mydash = require('lodash')
+
+// const items = [1,[2,[3,[4]]]]
+
+// const newItems = mydash.flatMapDeep(items)
+// console.log(newItems)
+
+console.log90
